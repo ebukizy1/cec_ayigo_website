7715606a48a707eadb4cfe256f6cd78f70988a1f
chukuwuma4u2c <chukwuma4u2c@gmail.com>
2025-11-13 14:56:11 +0100

    updated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,6 +17,35 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+         {/* Google Analytics 4 */}
+         <script async src="https://www.googletagmanager.com/gtag/js?id=G-NL5JCQMFDM"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-NL5JCQMFDM');
+            `,
+          }}
+        />
+        <Script id="tiktok-pixel" strategy="afterInteractive">
+  {`
+    !function (w, d, t) {
+      w.TiktokAnalyticsObject = t; var ttq = w[t] = w[t] || [];
+      ttq.methods = ["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"];
+      ttq.setAndDefer = function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};
+      for(var i=0;i<ttq.methods.length;i++){ttq.setAndDefer(ttq,ttq.methods[i])}
+      ttq.instance = function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++){ttq.setAndDefer(e,ttq.methods[n])}return e};
+      ttq.load = function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";
+      ttq._i=ttq._i||{}; ttq._i[e]=[]; ttq._i[e]._u=i; ttq._t=ttq._t||{}; ttq._t[e]=+new Date;
+      ttq._o=ttq._o||{}; ttq._o[e]=n||{}; var o=document.createElement("script"); o.type="text/javascript"; o.async=!0; o.src=i+"?sdkid="+e;
+      var a=document.getElementsByTagName("script")[0]; a.parentNode.insertBefore(o,a)};
+      ttq.load('D4AG1DRC77U1BLONRQ70'); ttq.page();
+    }(window, document, 'ttq');
+  `}
+</Script>
+
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="facebook-domain-verification" content="5zd5smm7qzfhr8f72w8gfrfpuuiym7" />
         {/* TikTok Pixel noscript fallback */}
